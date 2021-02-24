a3b28655af5903b2cdc2c9c11490a85836b26518
faizaslam11 <faizaslam11@gmail.com>
2021-02-24 22:19:51 +0530

    newww

--- src/SearchResult.js
+++ src/SearchResult.js
@@ -16,22 +16,22 @@ function SearchResult({
     return (
         <div className="searchResult">
             <img src={img} alt="" />
-            <FavoriteBorderIcon className="searchresult__heart"/>
+            <FavoriteBorderIcon className="searchResult__heart"/>
             <div className="searchResult__info">
                 <div className="searchResult__infoTop">
                 <p>{location}</p>
                 <h3>{title}</h3>
-                <p>______</p>
+                <p>__________</p>
                 <p>{description}</p>
 
                 </div>
 
 
                 <div classsName="searchResult__infoBottom">
-                     <div className="searchResultz__stars">
+                     <div className="searchResult__stars">
                      <StarIcon className="searchResult__star"/>
                      <p>
-                     <strong>{star}</strong> {price}
+                     <strong>{star}<br/>{price}</strong> 
                      </p>
                 </div>
         
